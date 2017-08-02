3619e45bee9d83be755fa3d8fc7df6b2259627d6
haizhiship <haizhiship@163.com>
2017-08-02 22:04:43 +0800

    spring boot的简单例子
    spring boot简单配置，响应页面后返回字符串

---
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	String home(){
+		return "Hello,spring boot!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
